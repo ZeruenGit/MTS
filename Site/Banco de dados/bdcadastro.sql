2a24226ced1b04714927d1012e1f92b765cc097e
Micro03 <teteuzym@gmail.com>
2019-08-28 08:41:49 -0300

    Segunda versão do git

--- Site/Banco de dados/bdcadastro.sql
+++ Site/Banco de dados/bdcadastro.sql
@@ -1,1 +1,2 @@
+create database bdcadastro;
 use bdcadastro;
--- Site/cadastro2.php
+++ Site/cadastro2.php
@@ -5,6 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/estilo.css">
 </head>
 <body>
     <?php
@@ -17,7 +21,7 @@
 
     $resultado = @mysqli_query($conexao, $sqlinsert);
 	if (!$resultado) {
-		die('Query Inválida: ' . @mysqli_error($conexao));
+		die('E-mail já existente: ' . @mysqli_error($conexao));
 	}
 	else {
         echo "Registro Cadastrado com Sucesso <br>";}
