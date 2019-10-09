914a0f149870adc4c3f5aef3a02d2dc12032b9c8
Micro02 <teteuzym@gmail.com>
2019-10-09 08:32:36 -0300

    new colors :D

--- Site/Banco de dados/bdcadastro.sql
+++ Site/Banco de dados/bdcadastro.sql
@@ -1,2 +1,53 @@
-create database bdcadastro;
+drop database if exists bdcadastro;
+create database if not exists bdcadastro;
 use bdcadastro;
+
+-- phpMyAdmin SQL Dump
+-- version 4.0.4
+-- http://www.phpmyadmin.net
+--
+-- Máquina: localhost
+-- Data de Criação: 26-Jun-2019 às 10:42
+-- Versão do servidor: 5.6.12-log
+-- versão do PHP: 5.4.12
+
+SET FOREIGN_KEY_CHECKS=0;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de Dados: `bdcadastro`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+CREATE TABLE IF NOT EXISTS `usuario` (
+  `email` varchar(200) NOT NULL,
+  `nome` varchar(200) NOT NULL,
+  PRIMARY KEY (`email`),
+  KEY `email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Truncate table before insert `usuario`
+--
+
+TRUNCATE TABLE `usuario`;SET FOREIGN_KEY_CHECKS=1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
